99ce2ad330e51cca3d9d170fc6735dec60fe121f
Macbook Pro <MacbookPro@Brunomasim.local>
2025-01-22 15:06:56 -0300

    oitavo

--- main.js/script.js
+++ main.js/script.js
@@ -1,26 +1,9 @@
-const galleryItems = document.querySelectorAll('.gallery-item');
-const modal = document.getElementById('modal');
-const modalImg = document.getElementById('modal-img');
-const modalDesc = document.getElementById('modal-desc');
-const closeBtn = document.querySelector('.close');
-
-// Exibir modal ao clicar em uma imagem
-galleryItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    modal.style.display = 'block';
-    modalImg.src = item.querySelector('img').src;
-    modalDesc.textContent = item.getAttribute('data-desc');
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".gallery-item");
+    items.forEach((item, index) => {
+      setTimeout(() => {
+        item.style.opacity = "1";
+        item.style.transform = "scale(1)";
+      }, index * 100); // Cria um atraso entre cada foto
+    });
   });
-});
-
-// Fechar modal ao clicar no botão de fechar
-closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-// Fechar modal ao clicar fora da imagem
-modal.addEventListener('click', (e) => {
-  if (e.target !== modalImg) {
-    modal.style.display = 'none';
-  }
-});
